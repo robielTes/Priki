a790850646d0b92a01b32c0155c8d7f21b689df9
robielcpnv <robiel.tesfazghi@cpnv.ch>
2021-12-16 09:08:21 +0100

    Update practice view to use laravel count function

--- resources/views/pratices/show.blade.php
+++ resources/views/pratices/show.blade.php
@@ -31,20 +31,20 @@
                         <p class="leading-relaxed">{{$opinion->description}}</p>
                        <div class="inline-grid grid-cols-3 gap-x-3">
                            <div class="flex flex-row">
-                               <p>{{count($opinion->useOpinion)}}</p>
+                               <p>{{$opinion->useOpinion->count()}}</p>
                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                    <path d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z" />
                                    <path d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z" />
                                </svg>
                            </div>
                            <div class="flex flex-row">
-                               <p>{{count($opinion->useOpinion->where('points','=',1))}}</p>
+                               <p>{{$opinion->useOpinion->where('points','=',1)->count()}}</p>
                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                    <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
                                </svg>
                            </div>
                            <div class="flex flex-row">
-                               <p>{{count($opinion->useOpinion->where('points','=',-1))}}</p>
+                               <p>{{$opinion->useOpinion->where('points','=',-1)->count()}}</p>
                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                    <path d="M18 9.5a1.5 1.5 0 11-3 0v-6a1.5 1.5 0 013 0v6zM14 9.667v-5.43a2 2 0 00-1.105-1.79l-.05-.025A4 4 0 0011.055 2H5.64a2 2 0 00-1.962 1.608l-1.2 6A2 2 0 004.44 12H8v4a2 2 0 002 2 1 1 0 001-1v-.667a4 4 0 01.8-2.4l1.4-1.866a4 4 0 00.8-2.4z" />
                                </svg>
@@ -52,7 +52,6 @@
                        </div>
                     </div>
                 </div>
-
                 @empty
                     <h3 class="text-center">Aucune opinion à afficher ici</h3>
                 @endforelse
