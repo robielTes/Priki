748335b90beedc764d0a6fedf5f1f015abff9603
robiel <robiel.tesfazghi@cpnv.ch>
2022-01-12 17:25:11 +0100

    Add 1000 character limite for comment

--- app/Http/Controllers/OpinionController.php
+++ app/Http/Controllers/OpinionController.php
@@ -6,35 +6,41 @@ use App\Models\Opinion;
 use App\Models\Practice;
 use App\Models\UserOpinion;
 use Illuminate\Http\Request;
+use \Illuminate\Http\RedirectResponse;
 
 class OpinionController extends Controller
 {
-    public function store(Request $request, int $id)
+    public function store(Request $request, int $id): RedirectResponse
     {
         Opinion::newOpinion($request, $id);
 
         return redirect()->route('practices.show', ['id' => $id]);
     }
 
-    public function destroy(Request $request, int $id, int $oId)
+    public function destroy(Request $request, int $id, int $oId): RedirectResponse
     {
         Opinion::find($oId)->delete();
 
         return redirect()->route('practices.show', ['id' => $id]);
     }
-    public function updateVote(Request $request, int $id, int $oId, int $vote)
+
+    public function updateVote(Request $request, int $id, int $oId, int $vote): RedirectResponse
     {
         UserOpinion::updateOrCreate(
-            ['opinion_id'=> $oId, 'user_id'=> auth()->user()->id],
+            ['opinion_id' => $oId, 'user_id' => auth()->user()->id],
             ['user_id' => auth()->user()->id, 'opinion_id' => $oId, 'comment' => "", 'points' => $vote]);
 
         return redirect()->route('practices.show', ['id' => $id]);
     }
-    public function updateComment(Request $request, int $id, int $oId)
+
+    public function updateComment(Request $request, int $id, int $oId): RedirectResponse
     {
+        $request->validate([
+            'comment' => ['required', 'max:1000', 'min:5'],
+        ]);
         UserOpinion::updateOrCreate(
-            ['opinion_id'=> $oId, 'user_id'=> auth()->user()->id],
-            ['user_id' => auth()->user()->id, 'opinion_id' => $oId, 'comment' => $_REQUEST['comment']]);
+            ['opinion_id' => $oId, 'user_id' => auth()->user()->id],
+            ['user_id' => auth()->user()->id, 'opinion_id' => $oId, 'comment' => $request->comment]);
 
         return redirect()->route('practices.show', ['id' => $id]);
     }
