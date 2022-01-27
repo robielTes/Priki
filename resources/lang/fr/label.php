59d2ea70ce4b7f90c9943f32ffd5839ddebcbe9b
robielcpnv <89458745+robielcpnv@users.noreply.github.com>
2022-01-27 09:09:23 +0100

    Update language for nav and header

--- resources/lang/fr/label.php
+++ resources/lang/fr/label.php
@@ -6,9 +6,10 @@ return[
     'Email'=> 'Email',
     'Password'=> 'Mot de passe',
     'ConfirmPassword'=> 'Confirmer le mot de passe',
-    'Register'=> 'Enregistrez',
+    'Register'=> 'Enregistrer',
     'AlreadyRegistered?'=> 'Déjà inscrit ?',
     'ForgotYourPassword?'=> 'Vous avez oublié votre mot de passe ?',
     'Login'=> 'Se connecter',
+    'Logout'=> 'Déconnexion',
     'Rememberme'=> 'Souviens-moi',
 ];
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -79,16 +79,18 @@
                         @csrf
                         <button
                             class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                            type="submit">Log Out
+                            type="submit">{{__('label.Logout')}}
                         </button>
                     </form>
                     @else
                     <a href="{{ route('login') }}"
-                       class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Log in</a>
+                       class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">{{__('label.Login')}}</a>
 
                     @if (Route::has('register'))
                         <a href="{{ route('register') }}"
-                           class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">Register</a>
+                           class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                            {{__('label.Register')}}
+                        </a>
                     @endif
                 @endauth
 
